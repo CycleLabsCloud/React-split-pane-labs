d7bb500525ea4a6c07cf390824326596446d9a1b
Filip Kolev <filip.kolev@tryonsolutions.com>
2022-01-07 16:10:11 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,54 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const sharedConfig = { resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss'],
+},
+module: {
+    rules: [
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            localIdentName: '[local]--[hash:base64:5]',
+                        },
+                    },
+                },
+                'sass-loader',
+            ],
+        },
+        {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: path.join(
+                            __dirname,
+                            'tsconfig.json',
+                        ),
+                        // dont compile tests into output
+                        // dont compile the just-messing-around file(s) like boot.custom.tsx into output
+                        onlyCompileBundledFiles: true,
+                    },
+                },
+            ],
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'source-map-loader',
+        },
+    ],
+}}
 function createClientConfig() {
     return {
+        ...sharedConfig,
         target: 'web',
         plugins: [
             new CopyPlugin({
@@ -25,52 +72,6 @@ function createClientConfig() {
             filename: '[name].js',
         },
         mode: 'development',
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss'],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        'style-loader',
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: {
-                                    localIdentName: '[local]--[hash:base64:5]',
-                                },
-                            },
-                        },
-                        'sass-loader',
-                    ],
-                },
-                {
-                    test: /\.ts(x?)$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'ts-loader',
-                            options: {
-                                configFile: path.join(
-                                    __dirname,
-                                    'tsconfig.json',
-                                ),
-                                // dont compile tests into output
-                                // dont compile the just-messing-around file(s) like boot.custom.tsx into output
-                                onlyCompileBundledFiles: true,
-                            },
-                        },
-                    ],
-                },
-                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-                {
-                    enforce: 'pre',
-                    test: /\.js$/,
-                    loader: 'source-map-loader',
-                },
-            ],
-        },
         // When importing a module whose path matches one of the following, just
         // assume a corresponding global variable exists and use that instead.
         // This is important because it allows us to avoid bundling all of our
@@ -87,7 +88,18 @@ function createClientConfig() {
 }
 function createLibraryConfig() {
     // return webpack config object for building the library
+    return {
+        ...sharedConfig,
+        output: {
+            library: {
+              name: 'React-split-Pane',
+              type: 'umd',
+            },
+            filename: 'bundle.js',
+          },
+         
   }
+}
 
 module.exports = (
     env,
